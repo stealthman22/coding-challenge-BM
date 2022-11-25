50bee89ba0d10bcf67372e945127ce61e89dd666
stealthman22 <tdotonline@gmail.com>
2022-11-25 23:59:14 +0000

    update

--- src/api/team/process.js
+++ src/api/team/process.js
@@ -1,7 +1,8 @@
 // ---------------------------------------------------------------------------------------------
 // YOU CAN FREELY MODIFY THE CODE BELOW IN ORDER TO COMPLETE THE TASK
 // ---------------------------------------------------------------------------------------------
+import player from "../../db/model/player";
 
 export default async (req, res) => {
-	res.send(`This is the team route`);
+	const bestPlayers = player.findAll({});
 };
